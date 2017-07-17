fc5f55b1f804bcb2bf93f02413a01c90a5f3118b
PratamaAgung <apratamamia@gmail.com>
2017-07-17 22:12:11 +0700

    Add connection to database, submit into database

--- mainApp.py
+++ mainApp.py
@@ -2,16 +2,55 @@ from flask import Flask, render_template, request, session, redirect, url_for, j
 import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 import json
+from datetime import datetime
 app = Flask(__name__)
 
+#database connection configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:12345@localhost/quotania'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+#model for quote
+class Quote(db.Model):
+    __tablename__ = "quotes"
+    id = db.Column(db.Integer, primary_key=True)
+    quote = db.Column(db.String(200))
+    author = db.Column(db.String(35))
+    nbLike = db.Column(db.Integer, default = 0)
+    date = db.Column(db.DateTime, default= datetime.today())
+    
+    def __init__(self, quote, author):
+        self.quote = quote
+        self.author = author
+
+#execute when running for the first time
+db.create_all()
+
+#routing for main page
 @app.route('/')
 @app.route('/index.html')
 def main_page():
     return render_template('index.html')
 
-@app.route('/submit_quote', methods = ['POST'])
+#routing for submit quote
+@app.route('/submit_quote', methods = ['GET', 'POST'])
 def submit_quote():
-	return jsonify(status = True);
+	try:
+		my_quote = Quote(request.args.get('quote', '', type = str), 
+			request.args.get('author', '', type = str))
+		db.session.add(my_quote)
+		db.session.commit()
+		return jsonify(status = True)
+	except:
+		return jsonify(status = False)
+
+
+#ruoting for getting quote
+@app.route('/get_quote_after', methods = ['GET', 'POST'])
+def get_quote_after():
+	quotes = Quote.query.order_by(Quote.nbLike.desc()).all()
+	print(list(quotes))
+	return jsonify(status = True)
 
 if __name__=="__main__":
     app.jinja_env.cache = {}
--- static/js/quotaniaApp.js
+++ static/js/quotaniaApp.js
@@ -1,24 +1,35 @@
 var quotApp = angular.module('quotania', []);
-quotApp.controller('quotController', function($scope, $http){
+quotApp.controller('submitController', function($scope, $http){
 	$scope.submitStatus = 0;
-	$scope.quote = {quo:"This is my quote", auth:"And I'm the author"};
 	$scope.submit = function() {
-		$scope.quote = $scope.submit;
-		try{
-			$http({
-	        	url : submit_quote_url,
-	        	method : 'POST',
-	        	params : {quote : $scope.submit.quo, author : $scope.submit.auth}
-	        }).then(function(response) {
-	        	if (response.status){
-	        		$scope.submitStatus = 1;
-	        	} else {
-	        		$scope.submitStatus = -1;
-	        	}
-	        });
-		} catch(err) {
-			$scope.submitStatus = -1;
+		if ($scope.submit.quo == '' || $scope.submit.auth == '') {
+			$scope.submitStatus = 2;
+		} else {
+			try{
+				$http({
+		        	url : submit_quote_url,
+		        	method : 'POST',
+		        	params : {quote : $scope.submit.quo, author : $scope.submit.auth}
+		        }).then(function(response) {
+		        	if (response.status){
+		        		$scope.submitStatus = 1;
+		        	} else {
+		        		$scope.submitStatus = -1;
+		        	}
+		        });
+			} catch(err) {
+				$scope.submitStatus = -1;
+			}
 		}
-        $scope.submit = null;
+        $scope.submit.quo = '';
+        $scope.submit.auth = '';
 	};
+});
+
+quotApp.controller('viewController', function($scope, $http){
+	$http({
+		url : get_quote_after_url,
+		method : 'POST',
+		params : {}
+	});
 });
